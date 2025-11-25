9dc78ab876b300b72d2345c0716a90589ee02b81
Vertrowd <rudrapratapsinghind42002@gmail.com>
2025-11-26 01:41:15 +0530

    W_Mirror_Array-optimal solution

--- W_Mirror_Array.java
+++ W_Mirror_Array.java
@@ -2,33 +2,31 @@ import java.util.Scanner;
 
 public class W_Mirror_Array {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        int m=sc.nextInt();
-        int[][] arr = new int[n][n];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                arr[i][j]=sc.nextInt();
-            }
-        }
-        
-        int[][] temp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                temp[i][j] = arr[i][j];
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = sc.nextInt();
             }
         }
 
         for (int i = 0; i < n; i++) {
-            int count=1;
-            for (int j = 0; j <m; j++) {
-                temp[i][j]=arr[i][m-count];
-                count++;
+            for (int j = 0; j < m / 2; j++) {   // swap
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][m - j - 1];
+                arr[i][m - j - 1] = temp;
             }
         }
+
+   
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(temp[i][j]+" ");
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
