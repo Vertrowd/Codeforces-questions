6d53f69fd7580513dcd054d5b833ae13bee08a69
Rudra <132250627+Vertrowd@users.noreply.github.com>
2026-01-15 10:00:23 +0530

    Hard Compare

---
+++ Hard_Compare.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    double r1 = b * log(a);
+    double r2 = d * log(c);
+
+    if (r1 > r2)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
